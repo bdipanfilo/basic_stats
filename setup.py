0f7892736283647518a09857fe484a11c085f22e
bdipanfilo <bdipanfilo@uri.edu>
2021-12-01 14:18:21 -0500

    some updates

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = "basic_stats",
+    version = "0.1",
+    author = "Brandon DiPanfilo",
+    packages=['basic_stats'],
+    install_requires=['numpy','scipy.io'],
+)
